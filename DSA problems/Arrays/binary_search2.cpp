1171b3529f878292e68a080d51681909d4be0f26
Mohit kashyap <mohitcode.cpp@gmail.com>
2026-02-27 14:13:10 +0530

    binary seach - To find the peak element

--- DSA problems/Arrays/binary_search2.cpp
+++ DSA problems/Arrays/binary_search2.cpp
@@ -3,19 +3,20 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+int peakElement(vector<int> nums){
+    int n=nums.size();
+    //binary search
+    int st=0; int end=n-1;
+    while(st<end){ int mid =st + (end -st)/2;
+
+        //if the element is on the right side
+        if(nums[mid]<nums[mid+1]){st=mid+1;
+        } /*means on the left side */else{end=mid;}
+    }
+    return st;
+}
 int main (){
-    int arr [] ={30,40,50,5,10,20};
-    int n =6;
-    int st=0; int end =n-1;int ans =0;
-    while(st<end){ int mid =st +(end-st )/2;
-        /* if the min number is on the right side*/
-        if(arr[mid] > arr[end]){
-            st=mid+1;
-        }else {
-            end=mid;
-        }
-       
-} 
-cout<< arr[st];
-return 0;
+    vector<int> nums ={1,2,3,78,8,2};
+   cout<< "peak element is on the index   " << peakElement(nums)<<" ";
+   return 0;
 }
